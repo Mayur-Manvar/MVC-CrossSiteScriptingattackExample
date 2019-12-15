8a529bb0225329b725d811c2a800e098ab06e736
Mayur-Manvar <mayurpmanvar@gmail.com>
2019-12-16 05:09:12 +0530

    Add demo first time.

---
+++ MVCCrossSideScriptingAttact/Controllers/CommentsController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCCrossSideScriptingAttact.Models;
+using System.Text;
+
+namespace MVCCrossSideScriptingAttact.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private EmployeeEntities db = new EmployeeEntities();
+
+        // GET: Comments
+        public ActionResult Index()
+        {
+            return View(db.tblComments.ToList());
+        }
+
+        // GET: Comments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblComment tblComment = db.tblComments.Find(id);
+            if (tblComment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblComment);
+        }
+
+        // GET: Comments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Comments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateInput(false)]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Comments")] tblComment tblComment)
+        {
+            StringBuilder sbComments = new StringBuilder();
+
+            // Encode the text that is coming from comments textbox
+            sbComments.Append(HttpUtility.HtmlEncode(tblComment.Comments));
+
+            // Only decode bold and underline tags
+            sbComments.Replace("&lt;b&gt;", "<b>");
+            sbComments.Replace("&lt;/b&gt;", "</b>");
+            sbComments.Replace("&lt;u&gt;", "<u>");
+            sbComments.Replace("&lt;/u&gt;", "</u>");
+            tblComment.Comments = sbComments.ToString();
+
+
+            if (ModelState.IsValid)
+            {
+                db.tblComments.Add(tblComment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblComment);
+        }
+
+        // GET: Comments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblComment tblComment = db.tblComments.Find(id);
+            if (tblComment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblComment);
+        }
+
+        // POST: Comments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Comments")] tblComment tblComment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblComment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblComment);
+        }
+
+        // GET: Comments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblComment tblComment = db.tblComments.Find(id);
+            if (tblComment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblComment);
+        }
+
+        // POST: Comments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblComment tblComment = db.tblComments.Find(id);
+            db.tblComments.Remove(tblComment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
